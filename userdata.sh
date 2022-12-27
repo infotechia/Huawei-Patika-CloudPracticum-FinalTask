9e318c0843ee88d8774078d1c70f3c1053ed2f6d
infotechia <93035119+infotechia@users.noreply.github.com>
2022-12-27 17:24:07 +0300

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -3,7 +3,7 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-TOKEN="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"
+TOKEN=${{ secrets.TOKEN }}
 git clone https://$TOKEN@https://github.com/infotechia/Huawei-Patika-CloudPracticum-FinalTask
 cd /home/ubuntu/Huawei-Patika-CloudPracticum-FinalTask
 apt install python3-pip -y
